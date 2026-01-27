d9eb0c98953a719f85c5a552507a3e20d894180f
Harry Fischer <harry.j.fischer@gmail.com>
2026-01-27 22:34:18 +0000

    add base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/harry-fischer-svelte-2025/',
   plugins: [svelte()],
 })
